619b619832ab43b310655f14361d48f587cb2ed6
JAZWii <jazwii2216@gmail.com>
2018-12-13 20:27:34 +0300

    improving rotation and thrusting

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,7 +7,9 @@ public class Rocket : MonoBehaviour
 	private Rigidbody _rigidbody;
 	private AudioSource _audioSource;
 	
-	
+	[SerializeField]float mainThrust = 100f;
+	[SerializeField]float rcsThrust = 100f;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -27,7 +29,7 @@ public class Rocket : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			_rigidbody.AddRelativeForce(Vector3.up);
+			_rigidbody.AddRelativeForce(Vector3.up * mainThrust);
 			if (!_audioSource.isPlaying)
 				_audioSource.Play();
 		}
@@ -40,20 +42,37 @@ public class Rocket : MonoBehaviour
 	private void Rotate()
 	{
 		_rigidbody.freezeRotation = true;
-		
+
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		
 		if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(-Vector3.forward);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
 		
 		_rigidbody.freezeRotation = false;
 
 	}
 
-	
+	private void OnCollisionEnter(Collision collision)
+	{
+		switch (collision.gameObject.tag)
+		{
+			case "Friendly":
+				
+				break;
+			case "Fuel":
+				
+				break;
+			default:
+				
+				break;
+			
+		}
+	}
 }
