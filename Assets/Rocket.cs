78b0853f2bcabbcf4c19a2ad1053e0e51abaa01c
JAZWii <jazwii2216@gmail.com>
2018-12-17 22:27:20 +0300

    sounds, lights, code has been modified

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,45 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang.Runtime.DynamicDispatching;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 	private Rigidbody _rigidbody;
 	private AudioSource _audioSource;
-	
+	private State _state = State.Alive;
+
 	[SerializeField]float mainThrust = 100f;
 	[SerializeField]float rcsThrust = 100f;
+	[SerializeField]AudioClip mainEngine;
+	[SerializeField]AudioClip success;
+	[SerializeField]AudioClip death;
 
+	enum State { Alive, Dying, Transcending}
+	
 	// Use this for initialization
 	void Start ()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
 		_audioSource = GetComponent<AudioSource>();
-
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Thrust();
-		Rotate();
+		if (_state == State.Alive)
+		{
+			RespondToThrustInput();
+			RespondToRotateInput();
+		}
 	}
 
-	private void Thrust()
+	private void RespondToThrustInput()
 	{
 		if (Input.GetKey(KeyCode.Space))
 		{
-			_rigidbody.AddRelativeForce(Vector3.up * mainThrust);
-			if (!_audioSource.isPlaying)
-				_audioSource.Play();
+			ApplyThrust();
 		}
 		else
 		{
 			_audioSource.Stop();
 		}
 	}
-	
-	private void Rotate()
+
+	private void ApplyThrust()
+	{
+		_rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+		if (!_audioSource.isPlaying)
+			_audioSource.PlayOneShot(mainEngine);
+	}
+
+	private void RespondToRotateInput()
 	{
 		_rigidbody.freezeRotation = true;
 
@@ -56,11 +72,13 @@ public class Rocket : MonoBehaviour
 		}
 		
 		_rigidbody.freezeRotation = false;
-
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
+		if(_state != State.Alive)
+			return;
+		
 		switch (collision.gameObject.tag)
 		{
 			case "Friendly":
@@ -69,10 +87,39 @@ public class Rocket : MonoBehaviour
 			case "Fuel":
 				
 				break;
-			default:
-				
+			case "Finish":
+				StartSuccessSequence();
 				break;
-			
+			default:
+				StartDeathSequence();
+				break;
 		}
 	}
+
+	private void StartSuccessSequence()
+	{
+		_state = State.Transcending;
+		_audioSource.Stop();
+		_audioSource.PlayOneShot(success);
+		LoadScene(SceneManager.GetActiveScene().buildIndex + 1, _state);
+	}
+	
+	private void StartDeathSequence()
+	{
+		_state = State.Dying;
+		_audioSource.Stop();
+		_audioSource.PlayOneShot(death);
+		LoadScene(SceneManager.GetActiveScene().buildIndex, _state);
+	}
+
+	void LoadScene(int sceneNumber,State state, float delayTime=1f)
+	{
+		StartCoroutine(LoadSceneIEnum(sceneNumber,state, delayTime));
+	}
+	
+	IEnumerator LoadSceneIEnum(int sceneNumber,State state, float delayTime=0f)
+	{
+		yield return new WaitForSeconds(delayTime);
+		SceneManager.LoadScene(sceneNumber);
+	}	
 }
