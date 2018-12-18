4d2705523f2a207674fb291dc325e1ec741600bf
JAZWii <jazwii2216@gmail.com>
2018-12-18 18:03:34 +0300

    C# Script Oscillator, to move objects

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//To prevent multiple scripts 
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+	[SerializeField] Vector3 movementVector;
+
+	[Range(0,1)][SerializeField]float movementFactor;
+
+	private Vector3 _startPosition;
+	// Use this for initialization
+	void Start ()
+	{
+		_startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 offeset = movementVector * movementFactor;
+		transform.position = _startPosition + offeset;
+	}
+}
